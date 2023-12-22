1093cf630994065e20ee9ebcfa63801214bf9052
naheda31 <142994348+naheda31@users.noreply.github.com>
2023-12-22 12:13:30 +0530

    Delete moviesapp directory

--- moviesapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . models import Movies
-
-
-
-class Moviesserializers(serializers.ModelSerializer):
-    class Meta:
-        model=Movies
-        fields=['id','name','language','hero']
